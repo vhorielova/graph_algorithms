46c448aee271130cb155296b2dabca088bb4694c
vhorielova <horielova.viktoria@gmail.com>
2025-02-26 17:50:28 +0200

    test dfs

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from graph import Graph
+from dfs import dfs
 
 numberOfVertices = 4
 edges = [[1, 2], [2, 3], [1, 3], [3, 4]]
 isOriented = False
 graph = Graph(numberOfVertices, edges, isOriented)
 print(graph.__str__())
+dfs(graph, 2)
